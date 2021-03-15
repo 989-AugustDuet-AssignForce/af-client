df0a29028f62e0d218b9de5664ca4e707bef864a
Ulric Todman <rick.sinclair.nyc@gmail.com>
2021-03-15 16:43:04 -0400

    commit to pull latest changes

--- af-client/src/app/services/room.service.spec.ts
+++ af-client/src/app/services/room.service.spec.ts
@@ -1,8 +1,11 @@
+import { HttpResponse } from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { POINT_CONVERSION_UNCOMPRESSED } from 'node:constants';
+import { RoomRequestDto } from '../models/room-request-dto';
 
 import { RoomService } from './room.service';
 
@@ -18,55 +21,88 @@ describe('RoomService', () => {
     httpMock = TestBed.inject(HttpTestingController);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-  // it('should get all rooms', () => {
-  //   const roomsObservable = service.getAllRooms();
-  //   let response;
+  it('should provide an array of all rooms', () => {
+    const roomsObservable = service.getAllRooms();
+    let response;
 
-  //   roomsObservable.subscribe((res) => {
-  //     response = res;
-  //   });
+    roomsObservable.subscribe((res) => {
+      response = res;
+    });
 
-  //   expect(response).toBeInstanceOf(Array);
-  // });
+    expect(response).toBeInstanceOf(Array);
+  });
 
-  // it('should provide a list of all remote training rooms', () => {
-  //   const trainingRoomsObservable = service.getAllRemoteTrainingRooms();
-  //   let response;
+  it('should provide an array of all remote training rooms', () => {
+    const trainingRoomsObservable = service.getAllRemoteTrainingRooms();
+    let response;
 
-  //   trainingRoomsObservable.subscribe((res) => {
-  //     response = res;
-  //   });
-  //   expect(response).toBeInstanceOf(Array);
-  // });
+    trainingRoomsObservable.subscribe((res) => {
+      response = res;
+    });
+    expect(response).toBeInstanceOf(Array);
+  });
 
-  // it('should provide a list of all remote meeting rooms', () => {
-  //   const meetingRoomsObservable = service.getAllRemoteMeetingRooms();
-  //   let response;
+  it('should provide an array of all remote meeting rooms', () => {
+    const meetingRoomsObservable = service.getAllRemoteMeetingRooms();
+    let response;
 
-  //   meetingRoomsObservable.subscribe((res) => {
-  //     response = res;
-  //   });
-  //   expect(response).toBeInstanceOf(Array);
-  // });
+    meetingRoomsObservable.subscribe((res) => {
+      response = res;
+    });
+    expect(response).toBeInstanceOf(Array);
+  });
 
-  // it('should provide a list of all physical meeting rooms', () => {
-  //   const physicalMeetingRoomsObservable = service.getAllPhysicalMeetingRooms();
-  //   let response;
+  it('should provide a array of all physical meeting rooms', () => {
+    const physicalMeetingRoomsObservable = service.getAllPhysicalMeetingRooms();
+    let response;
 
-  //   physicalMeetingRoomsObservable.subscribe((res) => {
-  //     response = res;
-  //   });
-  // });
+    physicalMeetingRoomsObservable.subscribe((res) => {
+      response = res;
+    });
+  });
 
-  // it('should provide a list of all physical training rooms', () => {
-  //   const physicalTrainingRoomsObservable = service.getAllPhysicalTrainingRooms();
-  //   let response;
-  //   physicalTrainingRoomsObservable.subscribe((res) => {
-  //     response = res;
-  //   });
-  // });
+  it('should provide an array of all physical training rooms', () => {
+    const physicalTrainingRoomsObservable = service.getAllPhysicalTrainingRooms();
+    let response;
+
+    physicalTrainingRoomsObservable.subscribe((res) => {
+      response = res;
+    });
+
+    expect(response).toBeInstanceOf(Array);
+  });
+
+  it('should provide an array of all training rooms', () => {
+    const allTrainingRoomsObservable = service.getTrainingRooms();
+    let response;
+    allTrainingRoomsObservable.subscribe((res) => {
+      response = res;
+    });
+  });
+
+  it('should update an existing room', () => {
+    const expected = new HttpResponse({ body: '', status: 204 });
+    const updateRoomObservable = service.updateRoom(
+      1,
+      new RoomRequestDto(
+        'Test Room A',
+        'REMOTE',
+        'MEETING',
+        20,
+        2,
+        new Set(['Projector', 'STREAMING'])
+      )
+    );
+
+    // const request = updateRoomObservable.subscribe(
+    //   (response: HttpResponse<any>) => {
+    //     expect(response).toEqual(expected);
+    //   }
+    // );
+    // request.unsubscribe();
+  });
 });
